7042194aefd58d6b69659ec9e4f66313c7da60a2
68011101-suppalerk <68011101@kmitl.ac.th>
2026-03-25 10:41:15 +0700

    pls4

---
+++ src/purchases/purchase.interface.ts
@@ -0,0 +1,18 @@
+export interface Purchase {
+    id: number,
+    customerName: string,
+    purchaseDate: string,
+    items: [
+      {
+        productId: number,
+        quantity: number,
+        price: number
+      },
+      {
+        productId: number,
+        quantity: number,
+        price: number
+      }
+    ],
+    totalPrice: number
+  }
